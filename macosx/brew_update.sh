f45f063fdc260089ff788f80943440f77f97c910
Marco Baldelli <marcobaldelli@users.noreply.github.com>
2018-02-06 17:18:07 +0100

    Add absolute path of brew for cron jobs

--- macosx/brew_update.sh
+++ macosx/brew_update.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -eu -o pipefail
+brew=/usr/local/bin/brew
 
 # Update source packages
-brew update
-brew upgrade
-brew cleanup
+$brew update
+$brew upgrade
+$brew cleanup
 
 # Update binary packages
-brew cask upgrade --greedy
-brew cask cleanup
+$brew cask upgrade --greedy
+$brew cask cleanup
