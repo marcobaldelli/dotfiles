fb496d3b53f781db30ebe452f889a63af292a3ff
Marco Baldelli <marcobaldelli@users.noreply.github.com>
2018-09-15 09:26:10 +0200

    Calling `brew cask cleanup` is now deprecated

--- macosx/brew_update.sh
+++ macosx/brew_update.sh
@@ -5,8 +5,9 @@ brew=/usr/local/bin/brew
 # Update source packages
 $brew update
 $brew upgrade
-$brew cleanup
 
 # Update binary packages
 $brew cask upgrade --greedy
-$brew cask cleanup
+
+# Clean up
+$brew cleanup
