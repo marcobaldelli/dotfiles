385bb5339a60fbe1809f9c888e609edd07bd8c19
Marco Baldelli <marcobaldelli@users.noreply.github.com>
2018-09-30 21:31:15 +0200

    OSX: Fix font rendering for non-retina displays (since macOS 10.14 Mojave)

--- macosx/osxdefaults.sh
+++ macosx/osxdefaults.sh
@@ -3,3 +3,7 @@ set -eu -o pipefail
 
 # Terminal: disable "Show Mark" option
 defaults write com.apple.Terminal ShowLineMarks -int 0
+
+# Fix font rendering for non-retina displays (since macOS 10.14 Mojave)
+defaults write -globalDomain AppleFontSmoothing -int 3
+defaults write -g CGFontRenderingFontSmoothingDisabled -bool false
