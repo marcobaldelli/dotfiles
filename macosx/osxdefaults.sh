49b437e1a7d4c43099cfee099b02c12b74d001d6
Marco Baldelli <marcobaldelli@users.noreply.github.com>
2019-08-11 11:11:49 +0200

    macOS Terminal: disable "bash sessions" feature

--- macosx/osxdefaults.sh
+++ macosx/osxdefaults.sh
@@ -4,3 +4,5 @@ set -eu -o pipefail
 # Terminal: disable "Show Mark" option
 defaults write com.apple.Terminal ShowLineMarks -int 0
 
+# Terminal: disable "bash sessions" feature
+touch "$HOME/.bash_sessions_disable"
