debb2ec5a5157e3b84cda990d0640102f0967ed7
Marco Baldelli <marcobaldelli@users.noreply.github.com>
2019-08-11 11:10:50 +0200

    macOS: disable non-retina display stuff
    (I now have a retina display)

--- macosx/osxdefaults.sh
+++ macosx/osxdefaults.sh
@@ -4,6 +4,3 @@ set -eu -o pipefail
 # Terminal: disable "Show Mark" option
 defaults write com.apple.Terminal ShowLineMarks -int 0
 
-# Fix font rendering for non-retina displays (since macOS 10.14 Mojave)
-defaults write -globalDomain AppleFontSmoothing -int 3
-defaults write -g CGFontRenderingFontSmoothingDisabled -bool false
